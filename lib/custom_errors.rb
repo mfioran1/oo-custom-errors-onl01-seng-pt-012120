293d4a71ca5744425ba4c005fb72209f0d8ff924
Maurice Fiorani <mauricef97@gmail.com>
2020-02-24 19:03:42 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,6 +8,15 @@ class Person
   def get_married(person)
     self.partner = person
     person.partner = self
+    if person.class != Person 
+      raise PartnerError
+    else
+      person.partner = self 
+    end
+  end
+  
+  class PartnerError < StandardError
+    
   end
 
 end
